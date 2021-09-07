a99fa039f62ecb2fa61e8c0f7d8d169a651bec88
chae won <80037363+chony-y@users.noreply.github.com>
2021-09-07 22:53:22 +0200

    11th version

--- wunsch.js
+++ wunsch.js
@@ -1,7 +1,21 @@
-window.addEventListener("scroll", function(){     
+window.addEventListener("scroll", function(){   
+	
+	var explain = document.getElementById('wunsch-top');
+	var write = document.querySelector('main');
 
-	const explain = document.getElementById('wunschmain');
-	explain.classList.wunschmain('show', window.scrollY > 0);
+	if(window.scrollY < 0) {
+		explain.style.display = 'block';
+		write.style.display = 'none';
+	}
+	else {
+		explain.style.display = 'none';
+		write.style.display = 'block';
+	}
 
-	console.log('스크롤');
 });
+
+
+window.addEventListener('load', () => {  //새로고침 했을 때 맨 위로 올리는거. 좀 늦춰져야 잘 동작해서 setTimeout으로 함수 만들어준거 
+	setTimeout(() => scrollTo(0, 0), 100);
+});
+
