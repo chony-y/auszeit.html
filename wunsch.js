4352275c4457d75c5760c0004fc5ca23f9fb6721
chae won <80037363+chony-y@users.noreply.github.com>
2021-09-08 12:18:24 +0200

    11th version

--- wunsch.js
+++ wunsch.js
@@ -1,21 +1,30 @@
-window.addEventListener("scroll", function(){   
-	
-	var explain = document.getElementById('wunsch-top');
-	var write = document.querySelector('main');
+window.addEventListener("scroll", () => {   
+	let mouse = document.getElementsByClassName('mouse-scroll2');
 
 	if(window.scrollY < 0) {
-		explain.style.display = 'block';
-		write.style.display = 'none';
+		document.getElementsByClassName('mouse-scroll2').style.display = 'none';
 	}
-	else if (window.scrollY > 0) {
-		explain.style.display = 'none';
-		write.style.display = 'block';
-	}
-
 });
 
+// window.addEventListener("scroll", () => {   
+
+// 	const scrolled = window.scrollY;
+	
+// 	var explain = document.getElementById('wunsch-top');
+// 	var write = document.querySelector('main');
+
+// 	if(scrolled < 0) {
+// 		explain.style.display = 'block';
+// 		write.style.display = 'none';
+// 	}
+// 	else {
+// 		explain.style.display = 'none';
+// 		write.style.display = 'block';
+// 	}
+
+// });
+//window.scrollTo(x, y); 얘는 어떻게 쓰지
 
 window.addEventListener('load', () => {  //새로고침 했을 때 맨 위로 올리는거. 좀 늦춰져야 잘 동작해서 setTimeout으로 함수 만들어준거 
 	setTimeout(() => scrollTo(0, 0), 100);
 });
-
