177300b1929bd05c1a92108529046347b92b3e7e
chae won <80037363+chony-y@users.noreply.github.com>
2021-09-12 21:48:46 +0200

    13th version
    meditation site too

--- meditation.js
+++ meditation.js
@@ -8,11 +8,27 @@ left.addEventListener('mouseenter', () => {
 left.addEventListener('mouseleave', () => {
 	container.classList.remove('hover-left');
 });
-
-
 right.addEventListener('mouseenter', () => {
 	container.classList.add('hover-right');
 });
 right.addEventListener('mouseleave', () => {
 	container.classList.remove('hover-right');
+});
+
+
+const up = document.querySelector('.up');
+const down = document.querySelector('.down');
+// const container = document.querySelector('.container');
+
+up.addEventListener('mouseenter', () => {
+	container.classList.add('hover-up');
+});
+up.addEventListener('mouseleave', () => {
+	container.classList.remove('hover-up');
+});
+down.addEventListener('mouseenter', () => {
+	container.classList.add('hover-down');
+});
+down.addEventListener('mouseleave', () => {
+	container.classList.remove('hover-down');
 });
