c89aca7c99c9143b2dbe5f3ec1a9eaf404491fb5
chae won <80037363+chony-y@users.noreply.github.com>
2021-09-15 19:27:22 +0200

    14th version
    changed image size
    added a favicon

--- mysql.js
+++ mysql.js
@@ -1,9 +1,34 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'blabla',
-  database : 'my_db'
+  password : 'cyoon0918',
+  database : 'wunsch'
 });
  
+
+// mysql 연결
+// com.microsoft.sqlserver.jdbc.SQLServerDriver;
+
+
 connection.connect();
+
+var sql = "SELECT * FROM wunsche";
+var inserts = ['wish', 'name', 'date'];
+sql = mysql.format(sql, inserts);
+
+connection.query('SELECT * FROM wunsche', function(error, results, fields) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(fields);
+});
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query('SELECT * FROM wunsche', function (err, result, fields) {
+//     if (err) throw err;
+//     console.log(fields);
+//   });
+// });
+
+connection.end();
